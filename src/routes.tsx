937934bc7afad7c461a2fd3ea615e94af738b9ec
Danrlei <danrleidalfre@gmail.com>
2024-03-04 19:24:15 -0300

    add sign up page

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,6 +4,7 @@ import { AppLayout } from '@/layouts/app.tsx'
 import { AuthLayout } from '@/layouts/auth.tsx'
 import { Dashboard } from '@/pages/app/dashboard.tsx'
 import { SignIn } from '@/pages/auth/sign-in.tsx'
+import { SignUp } from '@/pages/auth/sign-up.tsx'
 
 export const router = createBrowserRouter([
   {
@@ -17,12 +18,16 @@ export const router = createBrowserRouter([
     ],
   },
   {
-    path: '/sign-in',
+    path: '/',
     element: <AuthLayout />,
     children: [
       {
         path: '/sign-in',
         element: <SignIn />,
+      },
+      {
+        path: '/sign-up',
+        element: <SignUp />,
       },
     ],
   },
